bd312a6461279d3d455dfa990455986c8983af12
Kevin Yael <kevinyael2010@gmail.com>
2023-04-07 11:47:32 -0600

    Se mejoro la logica de combate y se agrego dinamicas a la pagina

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,15 @@
 let ataqueJugador 
 let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
 function iniciarJuego(){
+    //Oculta temporalmente secciones de la pagina
+    let seccionSelecionarAtaque = document.getElementById("ataque")
+    seccionSelecionarAtaque.style.display = 'none'
+    let seccionReiniciar = document.getElementById("reiniciar")
+    seccionReiniciar.style.display = 'none'
+
     let botonMascotaJugador = document.getElementById("boton-mascotas")
     botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
        
@@ -12,15 +20,24 @@ function iniciarJuego(){
     let botonPlanta = document.getElementById("boton-planta")
     botonPlanta.addEventListener('click', ataquePlanta)
 
+    let botonReiniciar = document.getElementById("reiniciar")
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+
 }
 
 //Selecion del usuario
 function selecionarMascotaJugador() {
+    //muestra la seccion de combate
+    let seccionSelecionarAtaque = document.getElementById("ataque")
+    seccionSelecionarAtaque.style.display = 'block'
+    //oculta los botones de mascotas
+    let seccionSelecionarMascota = document.getElementById("mascotas")
+    seccionSelecionarMascota.style.display = 'none'
+    
     let inputllamarizard = document.getElementById("llamarizard")
     let inputfloresaur = document.getElementById("floresaur")
     let inputtortoise = document.getElementById("tortoise")
     let spanmokeponJugador = document.getElementById('mokeponJugador')
-
 
     if (inputllamarizard.checked){
         alert("Selecionaste a Llamarizard")
@@ -84,18 +101,44 @@ function ataqueAleatorioEnemigo(){
 
 //logica de combate
 function combate(){
+    spanvidasJugador = document.getElementById('vidaJugador')
+    spanvidasEnemigo = document.getElementById('vidaEnemigo')
+
     if(ataqueEnemigo == ataqueJugador){
         crearMensaje("Empate")
+
     } else if(ataqueJugador == "Fuego" && ataqueEnemigo == "Planta"){
         crearMensaje("Ganaste 🎉")
+        vidasEnemigo--
+        spanvidasEnemigo.innerHTML = vidasEnemigo
+
     } else if(ataqueJugador == "Agua" && ataqueEnemigo == "Fuego"){
         crearMensaje("Ganaste 🎉")     
+        vidasEnemigo--
+        spanvidasEnemigo.innerHTML = vidasEnemigo
+
     } else if(ataqueJugador == "Planta" && ataqueEnemigo == "Agua"){
         crearMensaje("Ganaste 🎉")
+        vidasEnemigo--
+        spanvidasEnemigo.innerHTML = vidasEnemigo
+
     } else {
         crearMensaje("Perdiste 🤣")
+        vidasJugador--
+        spanvidasJugador.innerHTML = vidasJugador
+    }
+
+    revisarVidas()
+}
+
+function revisarVidas(){
+    if (vidasEnemigo == 0) {
+        crearMensajeFinal("Ganaste el combate")
+    }   else if (vidasJugador == 0) {
+        crearMensajeFinal("Perdiste el combate")
     }
 }
+
 
 //Historial de las acciones
 function crearMensaje(resultado){
@@ -105,6 +148,31 @@ function crearMensaje(resultado){
     parrafo.innerHTML = 'Tu Moképon ataco con ' + ataqueJugador + ', el Moképon del enemigo uso ' + ataqueEnemigo + ' - ' + resultado
 
     seccionMensaje.appendChild(parrafo)
+}
+
+function crearMensajeFinal(resultadoFinal){
+    let seccionMensaje = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    seccionMensaje.appendChild(parrafo)
+
+    //Deshabilita los botones de combate cuando el usuario/maquina llege a 0
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.disabled = true
+    let botonPlanta = document.getElementById("boton-planta")
+    botonPlanta.disabled = true
+
+    let seccionReiniciar = document.getElementById("reiniciar")
+    seccionReiniciar.style.display = 'block'
+
+}
+
+function reiniciarJuego(){
+    location.reload()
 }
 
 function aleatorio(min, max){
